df61c4ed077223cede6289e4ec03fd1215654c61
Austin Mwangi <stnmwangi@gmail.com>
2020-06-01 18:18:45 +0300

    Add prod and dev enviroments and icons for PWA

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,20 @@
-// load globals
-import { src, dest, watch, series, parallel, lastRun, task } from "gulp";
+import { src, dest, watch, series, parallel, task } from "gulp";
 import browserSync from "browser-sync";
 import del from "del";
 import autoprefixer from "autoprefixer";
 import cssnano from "cssnano";
 import loadPlugins from "gulp-load-plugins";
 
-// load all plugins
-const $ = loadPlugins({ lazy: false });
+// require("dotenv").config();
+
+// load all gulp-* plugins in package.json
+const plugin = loadPlugins();
 
 // initiate browserSync
 const bs = browserSync.create();
+
+// set environments
+const prodEnv = process.env.NODE_ENV === "production";
 
 // paths
 const paths = {
@@ -39,7 +43,12 @@ const paths = {
     src: "app/fonts/*",
     dist: "public/fonts/",
   },
-  dist: "public/",
+  configs: [
+    "app/apple-touch-icon.png",
+    "app/browserconfig.xml",
+    "app/manifest.json",
+  ],
+  dist: "public",
   maps: "/maps",
 };
 
@@ -67,82 +76,80 @@ const postCssPlugins = [
 // main styles
 export function styles() {
   return src(paths.styles.sass)
-    .pipe($.sourcemaps.init())
-    .pipe($.sass().on("error", $.sass.logError))
-    .pipe($.postcss(postCssPlugins))
-    .pipe($.sourcemaps.write(paths.maps))
+    .pipe(plugin.if(!prodEnv, plugin.sourcemaps.init()))
+    .pipe(plugin.sass().on("error", plugin.sass.logError))
+    .pipe(plugin.postcss(postCssPlugins))
+    .pipe(plugin.if(!prodEnv, plugin.sourcemaps.write(paths.maps)))
     .pipe(dest(paths.styles.dist))
-    .pipe($.size({ title: "main styles" }))
+    .pipe(plugin.size({ title: "main styles" }))
     .pipe(bs.stream());
 }
 
 // vendor styles
 export function vendorStyles() {
   return src(paths.styles.vendor)
-    .pipe($.sourcemaps.init())
-    .pipe($.postcss(postCssPlugins))
-    .pipe($.concat("libs.css"))
-    .pipe($.sourcemaps.write(paths.maps))
+    .pipe(plugin.if(!prodEnv, plugin.sourcemaps.init()))
+    .pipe(plugin.postcss(postCssPlugins))
+    .pipe(plugin.concat("libs.css"))
+    .pipe(plugin.if(!prodEnv, plugin.sourcemaps.write(paths.maps)))
     .pipe(dest(paths.styles.dist));
 }
 
 // vendor scripts
 export function vendorScripts() {
   return src(paths.scripts.vendor)
-    .pipe($.sourcemaps.init())
-    .pipe($.concat("libs.js"))
-    .pipe($.sourcemaps.write(paths.maps))
+    .pipe(plugin.if(!prodEnv, plugin.sourcemaps.init()))
+    .pipe(plugin.concat("libs.js"))
+    .pipe(plugin.if(!prodEnv, plugin.sourcemaps.write(paths.maps)))
     .pipe(dest(paths.scripts.dist))
-    .pipe($.size({ title: "vendor scripts" }));
+    .pipe(plugin.size({ title: "vendor scripts" }));
 }
 
 // main scripts
 export function mainScripts() {
   return src(paths.scripts.src)
-    .pipe($.sourcemaps.init())
-    .pipe($.babel())
-    .pipe($.concat("main.js"))
-    .pipe($.sourcemaps.write(paths.maps))
+    .pipe(plugin.if(!prodEnv, plugin.sourcemaps.init()))
+    .pipe(plugin.babel())
+    .pipe(plugin.concat("main.js"))
+    .pipe(plugin.if(!prodEnv, plugin.sourcemaps.write(paths.maps)))
     .pipe(dest(paths.scripts.dist))
-    .pipe($.size({ title: "main scripts" }));
+    .pipe(plugin.size({ title: "main scripts" }));
 }
 
 // lint main scripts
 export function lintScripts() {
   return src(paths.scripts.src)
     .pipe(
-      $.eslint({
+      plugin.eslint({
         useEslintrc: true,
       })
     )
-    .pipe($.eslint.format())
-    .pipe($.eslint.failAfterError());
+    .pipe(plugin.eslint.format())
+    .pipe(plugin.eslint.failAfterError());
 }
 
 // images
 export function images() {
-  return src(paths.images.src, { since: lastRun(images) })
+  return src(paths.images.src)
     .pipe(
-      $.cache(
-        $.imagemin([
-          $.imagemin.gifsicle({ interlaced: true }),
-          $.imagemin.mozjpeg({ quality: 100 }),
-          $.imagemin.optipng({ optimizationLevel: 5 }),
-          $.imagemin.svgo({
+      plugin.cache(
+        plugin.imagemin([
+          plugin.imagemin.gifsicle({ interlaced: true }),
+          plugin.imagemin.mozjpeg({ quality: 100 }),
+          plugin.imagemin.optipng({ optimizationLevel: 5 }),
+          plugin.imagemin.svgo({
             plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
           }),
         ])
       )
     )
     .pipe(dest(paths.images.dist))
-    .pipe($.size({ title: "images" }));
+    .pipe(plugin.size({ title: "images" }));
 }
 
 // fonts
 export function fonts() {
-  return src(paths.fonts.src, { since: lastRun(fonts) }).pipe(
-    dest(paths.fonts.dist)
-  );
+  return src(paths.fonts.src).pipe(dest(paths.fonts.dist));
 }
 
 // markup
@@ -150,26 +157,15 @@ export function mainViews() {
   return src(paths.views.src).pipe(dest(paths.views.dist));
 }
 
-// build output
-const build = series(
-  clean,
-  parallel(
-    styles,
-    vendorStyles,
-    mainScripts,
-    vendorScripts,
-    images,
-    fonts,
-    mainViews
-  )
-);
-task("build", build);
+// app configs
+export function appConfigs() {
+  return src(paths.configs).pipe(dest(paths.dist));
+}
 
 // watch file changes
-
 export function watchFiles() {
   bs.init({
-    server: ["app", "public"],
+    server: paths.dist,
     open: false,
     notify: false,
   });
@@ -183,11 +179,11 @@ export function watchFiles() {
   watch(paths.images.src, series(images)).on("change", bs.reload);
   watch(paths.fonts.src, series(fonts));
   watch(paths.views.src, series(mainViews)).on("change", bs.reload);
-  watch("./gulpfile.babel.js", series(build));
+  watch(paths.configs, series(appConfigs));
 }
 
-// develop tasks
-const develop = series(
+// tasks
+const build = series(
   clean,
   parallel(
     styles,
@@ -196,9 +192,14 @@ const develop = series(
     vendorScripts,
     images,
     fonts,
-    lintScripts,
     mainViews,
-    watchFiles
+    appConfigs
   )
 );
+task("build", build);
+
+const develop = series(build, parallel(watchFiles));
 task("dev", develop);
+
+const deploy = series(build);
+task("deploy", deploy);
